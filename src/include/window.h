cb9db6b10fd560402c5b5eb2d60cc24d7ff90433
Phil Chen <winkpoke@yahoo.com>
2021-02-04 16:49:53 +0800

    upgrade to imgui 1.80

--- src/include/window.h
+++ src/include/window.h
@@ -137,7 +137,7 @@ namespace ui {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
         //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-        //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 #endif
 
         // Create window with graphics context
